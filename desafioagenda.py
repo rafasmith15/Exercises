e0b21d591733601059a72acc102ec260a5dc6804
rafasmith15 <rafaelsmith88@outlook.com>
2024-05-13 14:40:56 -0300

    "# create class contato"

---
+++ desafioagenda.py
@@ -0,0 +1,7 @@
+class Contato:
+    def __init__(self, nome, telefone, email, favorito=False) -> None:
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.favorito = favorito
+
