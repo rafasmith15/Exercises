9a086cdc438acc75261a70567b811010cd494bfe
rafasmith15 <rafaelsmith88@outlook.com>
2024-05-13 15:00:12 -0300

    "# create class agenda"

--- desafioagenda.py
+++ desafioagenda.py
@@ -5,3 +5,13 @@ class Contato:
         self.email = email
         self.favorito = favorito
 
+class Agenda:
+    def __init__(self) -> None:
+        self.contatos = []
+
+    def add_contato(self, contato):
+        self.contatos.append(contato)   
+
+    def listar_contatos(self):
+        for contato in self.contatos:
+            print(f"Nome:{contato.nome}, Telefone:{contato.telefone}, Email:{contato.email},Favorito: {'Sim' if contato.favorito else 'Não'}")  
